a880f6656d4d49dda9aedd6232b27de917fad5e8
Wesley Moses <wesmoses@gmail.com>
2019-12-30 02:18:23 +0200

    Update App.js

--- App.js
+++ App.js
@@ -6,18 +6,7 @@ import { createStackNavigator } from 'react-navigation-stack'
 import Places from './Places'
 import Map from './Map'
 
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyDlNYpgjhIcf4f9Or5X2kYJ982AgHguAJM",
-  authDomain: "react-native-places-7dc5b.firebaseapp.com",
-  databaseURL: "https://react-native-places-7dc5b.firebaseio.com",
-  projectId: "react-native-places-7dc5b",
-  storageBucket: "react-native-places-7dc5b.appspot.com",
-  messagingSenderId: "847760272751",
-  appId: "1:847760272751:web:273af5f0b395f533508c9a"
-}
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(process.env.firebaseConfig)
 
 const { UIManager } = NativeModules
 
